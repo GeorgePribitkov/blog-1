4172ca37134867acea09d66e5aba64cb0334e074
GeorgePribitkov <zezulevi@gmail.com>
2024-03-10 11:47:34 +0300

    12
    12

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,36 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
+
+from .forms import CommentForm
+from django.views.decorators.http import require_POST
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post,
+                                                      'form': form,
+                                                      'comment': comment})
+
+
+
+class PostListView(ListView):
+    queryset = Post.objects.filter(status=Post.Status.PUBLISHED).all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
+
 
 
 # Create your views here.
@@ -32,5 +60,9 @@ def post_detail(request, year, month, day, post):
                               publish__month=month,
                               publish__day=day,
                               slug=post,)
-    return render(request, 'blog/post/list.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post/list.html', {'post': post,
+                                                   'comments': comments,
+                                                   'form': form})
 
